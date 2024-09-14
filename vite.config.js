db45eba7e2e290d7d526f71b4ce3af6ff437e8e5
weihlin <weihlin@cisco.com>
2024-09-14 16:24:53 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   html: {
-    cspNonce: 'nonce-12345',
+    cspNonce: '12345',
   },
   plugins: [vue()],
 })
